617e07fbb88d31cdb079287add4f1465adc90d49
Funbi Olowoyo <choppyolowoyo@outlook.com>
2025-08-12 22:00:36 -0500

    Update to VPC

--- vpc.tf
+++ vpc.tf
@@ -29,3 +29,37 @@ resource "aws_subnet" "test_subnet_az2" {
     Environment = "Testing"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "Terraform"
+    Environment = "Testing"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.test_vpc.id
+
+  tags = {
+    Name = "Terraform"
+    Environment = "Testing"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public_subnet_az1" {
+  subnet_id      = aws_subnet.test_subnet_az1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_az2" {
+  subnet_id      = aws_subnet.test_subnet_az2.id
+  route_table_id = aws_route_table.public_rt.id
+}
